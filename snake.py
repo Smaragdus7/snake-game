8c26d1a29019a7207395cee46cfb583a34b57c65
Clau Ibarra <Smaragdus7@users.noreply.github.com>
2022-06-17 14:23:33 -0500

    Update snake.py

--- snake.py
+++ snake.py
@@ -27,6 +27,8 @@ class Snake:
         self.segments.append(new_segment)
 
     def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
         self.segments.clear()
         self.create_snake()
         self.head = self.segments[0]
