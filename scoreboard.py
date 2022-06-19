26a4fe1c5083aa56a7aa4d8ca43790b6760ad73f
Clau Ibarra <Smaragdus7@users.noreply.github.com>
2022-06-18 20:26:40 -0500

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -5,6 +5,8 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.high_score = 0
         self.color("white")
         self.pu()
@@ -19,6 +21,8 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_score_board()
 
